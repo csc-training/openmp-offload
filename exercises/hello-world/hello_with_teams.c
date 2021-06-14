4dcb289053870a37dd428f794dedfbca49311148
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2021-06-14 18:40:15 +0300

    Create hello_with_teams.c

---
+++ exercises/hello-world/hello_with_teams.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#ifdef _OPENMP
+#include <omp.h>
+#endif
+
+/*
+• Device Runtime Support:
+•  To query the target environment
+omp_get_default_device
+omp_get_initial_device
+omp_get_num_devices
+omp_get_num_teams
+omp_get_team_num
+omp_is_initial_device
+omp_set_default_device
+•  To manage device memory
+omp_target_alloc
+omp_target_associate_ptr
+omp_target_disassociate_ptr
+omp_target_free
+omp_target_is_present
+omp_target_memcpy
+
+printf("%d %d %d %d %d. There are %d devices.  %d %d \n", omp_get_num_teams(), omp_get_team_num(), omp_is_initial_device(), omp_get_num_threads(), omp_get_num_devices(), omp_get_default_device(), omp_get_device_num());
+*/
+
+int main(void) {
+  int runningOnGPU = 0;
+#ifdef _OPENMP
+#pragma omp target device(1) map(tofrom:runningOnGPU)
+  {
+    if (omp_is_initial_device() == 0)
+      runningOnGPU = 1;
+      if (runningOnGPU)
+        {
+          printf(" ### Able to use the GPU! ### \n ");
+          printf("The default device is %d. There are %d devices available. Device %d is used.\n", omp_get_default_device(),  omp_get_num_devices(), omp_get_device_num());
+        }
+      else
+        {printf("### Unable to use the GPU, using CPU! ###\n");}
+  }
+  #else
+    printf("Code compiled without OpenMP\n");
+  #endif
+
+
+if (runningOnGPU)
+{
+#pragma omp target teams num_teams(5) device(1) map(from:runningOnGPU)
+  {
+    printf("Hello World!  \n The default device is %d. There are %d devices available. Device %d is used.\n", omp_get_default_device(),  omp_get_num_devices(), omp_get_device_num());
+    printf(" Team %d of %d teams.\n", omp_get_team_num(), omp_get_num_teams());
+  }
+}
+  return 0;
+}
