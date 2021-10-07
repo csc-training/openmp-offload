3ef5263a537d63053c64a6983fe77064953e4651
Jussi Enkovaara <jussi.enkovaara@csc.fi>
2021-10-07 11:08:13 +0300

    Batch script templates

---
+++ exercises/job_single.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --job-name=example
+#SBATCH --account=project_2000745
+#SBATCH --partition=gpumedium
+#SBATCH --reservation=openmp_offload
+#SBATCH --time=00:05:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:a100:1
+
+srun my_exe
